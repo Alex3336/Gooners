1d20f9b3334631c311d89fae36576735779f9a18
Софія Щербина <160267180+sofiascherbina@users.noreply.github.com>
2025-05-14 16:42:41 +0300

    finished sofia's part

--- js/footer.js
+++ js/footer.js
@@ -1,38 +1,20 @@
-(() => {
-    const modalGratitude = document.querySelector('.js-gratitude-container');
-    const openGratitude = document.querySelector('.js-open-gratitude');
-    const closeGratitude = document.querySelector('.js-close-gratitude');
-    let emailInput = document.querySelector('#footer-form-email');
-  
-    const toggleMenu = () => {
-      const isMenuOpen =
-        openGratitude.getAttribute('aria-expanded') === 'true' || false;
-      openGratitude.setAttribute('aria-expanded', !isMenuOpen);
-      if(emailInput.value){
-      modalGratitude.classList.toggle('is-open');
-  
-      const scrollLockMethod = !isMenuOpen
-        ? 'disableBodyScroll'
-        : 'enableBodyScroll';
-      bodyScrollLock[scrollLockMethod](document.body);
+const body = document.querySelector('body');
+const modalGratitude = document.querySelector('.js-gratitude-container');
+const openGratitude = document.querySelector('.js-open-gratitude');
+const closeGratitude = document.querySelector('.js-close-gratitude');
+let emailInput = document.querySelector('#footer-form-email');
+
+function hendlerClick () {
+    if(emailInput.value){
+       modalGratitude.classList.toggle('is-open');
+       body.style.overflow = 'hidden';
     }
-    else{
+  else{
       alert('Введіть вашу електронну адресу')
-    }
-    };
-  
-    openGratitude.addEventListener('click', toggleMenu);
-    closeGratitude.addEventListener('click', toggleMenu);
-  
-    // Close the mobile menu on wider screens if the device orientation changes
-    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-      if (!e.matches) return;
-      modalGratitude.classList.remove('is-open');
-      openGratitude.setAttribute('aria-expanded', false);
-      // bodyScrollLock.enableBodyScroll(document.body);
-    });
-
-  })();
-
-
-  
+  }
+};
+openGratitude.addEventListener('click', hendlerClick);
+closeGratitude.addEventListener('click', EVENT =>{
+  hendlerClick();
+  body.style.overflow = 'auto';
+} ); 
